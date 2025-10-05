a7047ce0e589945f2fa75816ab1aa1c294150f19
Dev_Faruq <olatoyesefaruq@gmail.com>
2025-10-06 00:25:49 +0100

    worked on pressable component

--- app/(auth)/login.jsx
+++ app/(auth)/login.jsx
@@ -1,16 +1,27 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Pressable, Text } from 'react-native'
 import ThemedView from '../../components/ThemedView'
 import Spacer from '../../components/Spacer'
 import ThemedText from '../../components/ThemedText'
 import { Link } from 'expo-router'
+import { Colors } from '../../constants/Colors'
+import ThemedButton from '../../components/ThemedButton'
 
 const Login = () => {
+
+ const handleSubmit = () => {
+     console.log('login pressed');
+ }
+
   return (
     <ThemedView style={styles.container}>
 
        <Spacer />
 
        <ThemedText title={true} style={styles.title}> Login to your account</ThemedText>
+
+       <ThemedButton onPress={handleSubmit}>
+         <Text style={{ color: '#f2f2f2' }}>Login</Text>
+       </ThemedButton>
 
        <Spacer height={100} />
       
@@ -27,11 +38,20 @@ export default Login
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center'
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     title: {
         textAlign: 'center',
         fontSize: 18,
         marginBottom: 30
     },
+    btn: {
+      backgroundColor: Colors.primary,
+      padding: 15,
+      borderRadius: 5
+    },
+    pressed: {
+      opacity: 0.8
+    }
 })
